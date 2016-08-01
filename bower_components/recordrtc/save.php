4543f09f2eaf29b023a315e077edf4eb865ac14d
Jose J. Lazo Leon <Jose J. Lazo Leon>
2016-08-01 15:07:23 +0200

    Error de subida a Twitter corregido (subida comentada). Actualización de librerías

--- bower_components/recordrtc/save.php
+++ bower_components/recordrtc/save.php
@@ -3,11 +3,10 @@
 if ( isset( $_POST['message'] ) ) {
   $message = $_POST['message'] . " por " . $_POST["nombreUsuario"];
 } else {
-  $message     = 'Probando la subida de vídeos con #PHP';//'Por qué me gustan las tortas de Inés Rosales #tortas #MeriendasAuténticas #MerienndasDeAntaño';
-  $nombreUsuario = $_POST["nombreUsuario"];
   // Definimos la variables de para compartir
-  $tags        = array("tortas", "merienda auténtica");
-  $title       = ( $nombreUsuario ) ? $nombreUsuario . ' con Inés Rosales' : 'Inés Rosales' ;
+  $message     = 'Probando la subida de vídeos con #PHP';
+  $tags        = array("PHP", "Castilla la Mancha");
+  $title       = ( isset( $_POST["nombreUsuario"] ) ) ? 'Video de ' . $_POST["nombreUsuario"] : 'Titulo del video' ;
   $description = $message;
 }
 
@@ -16,7 +15,7 @@ foreach(array('video', 'audio') as $type) {
 	if (isset($_FILES["${type}-blob"])) {
 		
 		$fileName 			 = $_POST["${type}-filename"];
-		$uploadDirectory = dirname(__FILE__).'/uploads/'.$fileName;
+		$uploadDirectory = dirname(__FILE__).'/uploads/' . $fileName;
 
 		if (!move_uploaded_file($_FILES["${type}-blob"]["tmp_name"], $uploadDirectory)) {
 			echo(" problem moving uploaded file");
@@ -26,13 +25,9 @@ foreach(array('video', 'audio') as $type) {
   }
 }
 
-if ( isset($nombreUsuario) ) {
-  echo "Nombre: " . $nombreUsuario . "<br>Mensaje:<br>" . $_POST['message'] . "<br>";
-}
-
 if ( !isset( $uploadDirectory ) ) {
   // die('No hay video adjunto');
-  $uploadDirectory = dirname(__FILE__).'/uploads/1469706044089.mp4';
+  $uploadDirectory = dirname(__FILE__) . '/uploads/1469706044089.mp4';
 }
 
 
@@ -134,18 +129,19 @@ $reply = $cb->media_upload([
 
 $media_id = $reply->media_id_string;
 
-echo "INIT:<br>";
-var_dump($reply);
+// echo "INIT:<br>";
+// var_dump($reply);
 
 // APPEND data to the upload
 
 $segment_id = 0;
 
 while (! feof($fp)) {
-  $chunk = fread($fp, 100000); // 1MB per chunk for this sample
+  $chunk = fread($fp, 1048576); // 1MB per chunk for this sample
 
   $reply = $cb->media_upload([
     'command'       => 'APPEND',
+    'media_type'    => 'video/mp4',
     'media_id'      => $media_id,
     'segment_index' => $segment_id,
     'media'         => $chunk
@@ -153,8 +149,8 @@ while (! feof($fp)) {
 
   $segment_id++;
 }
-echo "APPEND:<br>";
-var_dump($reply);
+// echo "APPEND:<br>";
+// var_dump($reply);
 
 fclose($fp);
 
@@ -162,31 +158,27 @@ fclose($fp);
 
 $reply = $cb->media_upload([
   'command'   => 'FINALIZE',
+  'media_type'=> 'video/mp4',
   'media_id'  => $media_id
 ]);
 
-echo "FINALIZE:<br>";
-var_dump($reply);
-
-// STATUS the upload
-
-$reply = $cb->media_upload([
-  'command'   => 'STATUS',
-  'media_id'  => $media_id
-]);
-
-echo "STATUS:<br>";
-var_dump($reply);
+// echo "FINALIZE:<br>";
+// var_dump($reply);
 
 if ($reply->httpstatus < 200 || $reply->httpstatus > 299) {
+  echo "Error de estado: " . $reply->httpstatus;
   die();
 }
-
+/**
+ *++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * ELINAR EL CÓDIGO COMENTADO PARA PODER PUBLICAR EN TWITTER +
+ *++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+*/
 // Usamos el media_id y el $message para tweetear
-$reply = $cb->statuses_update([
-  'status'    => $message,
-  'media_ids' => $media_id
-]);
+// $reply = $cb->statuses_update([
+//   'status'    => $message,
+//   'media_ids' => $media_id
+// ]);
 
 
 
@@ -202,10 +194,10 @@ $reply = $cb->statuses_update([
  * Please ensure that you have enabled the YouTube Data API for your project.
  */
 // Call set_include_path() as needed to point to your client library.
-set_include_path( get_include_path() . PATH_SEPARATOR . '/google-api-php-client/src');
-require_once 'Youtube/vendor/autoload.php';
-// require_once 'Google/Client.php';
-// require_once 'Google/Service/YouTube.php';
+// set_include_path( get_include_path() . PATH_SEPARATOR . '/google-api-php-client/src');
+require_once 'google-api-php-client/src/Google/autoload.php';
+require_once 'google-api-php-client/src/Google/Client.php';
+require_once 'google-api-php-client/src/Google/Service/YouTube.php';
 
 
 // Tokens 
@@ -213,44 +205,67 @@ require_once 'Youtube/vendor/autoload.php';
 // ';
 // $OAUTH2_CLIENT_SECRET = 'lmSj1WM-rSLLJu8Bh6z_aCvI';
 
+session_start();
+
+/*
+ * You can acquire an OAuth 2.0 client ID and client secret from the
+ * {{ Google Cloud Console }} <{{ https://cloud.google.com/console }}>
+ * For more information about using OAuth 2.0 to access Google APIs, please see:
+ * <https://developers.google.com/youtube/v3/guides/authentication>
+ * Please ensure that you have enabled the YouTube Data API for your project.
+ */
+// Tokens 
+$OAUTH2_CLIENT_ID     = 'totem-ines-rosales@appspot.gserviceaccount.com';
+$OAUTH2_CLIENT_SECRET = '02d5d3a0f8ba801eb59d645595e8325ba122e7c4';
+$token                = '';
+$REDIRECT             = filter_var('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'], FILTER_SANITIZE_URL);
+$APPNAME              = "Totem compartir video";
 
 $client = new Google_Client();
-$client->setAuthConfigFile('google-api-php-client/client_secret.json');
-$client->addScope(Google_Service_Drive::DRIVE_METADATA_READONLY);
-$client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php');
+$client->setClientId($OAUTH2_CLIENT_ID);
+$client->setClientSecret($OAUTH2_CLIENT_SECRET);
+$client->setScopes('https://www.googleapis.com/auth/youtube');
+$redirect = filter_var('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'],
+    FILTER_SANITIZE_URL);
+$client->setRedirectUri($redirect);
+
+// Define an object that will be used to make all API requests.
+$youtube = new Google_Service_YouTube($client);
 
 echo "<pre>";
 echo "<h1>Objeto Client</h1>";
-var_dump($client);die();
+var_dump($client);
+echo "<h1>Objeto Youtube</h1>";
+var_dump($youtube);
 echo "</pre>";
+die();
 
+// if( $client->getAccessToken() ) {
+//     $snippet = new Google_VideoSnippet();
+//     $snippet->setTitle( $title );
+//     $snippet->setDescription( $description );
+//     $snippet->setTags( $tags );
+//     $snippet->setCategoryId("22");
 
-if( $client->getAccessToken() ) {
-    $snippet = new Google_VideoSnippet();
-    $snippet->setTitle( $title );
-    $snippet->setDescription( $description );
-    $snippet->setTags( $tags );
-    $snippet->setCategoryId("22");
+//     $status = new Google_VideoStatus();
+//     $status->privacyStatus = "private";
 
-    $status = new Google_VideoStatus();
-    $status->privacyStatus = "private";
+//     $video = new Google_Video();
+//     $video->setSnippet($snippet);
+//     $video->setStatus($status);
 
-    $video = new Google_Video();
-    $video->setSnippet($snippet);
-    $video->setStatus($status);
+//     $error = true;
+//     $i = 0;
 
-    $error = true;
-    $i = 0;
-
-    try {
-        $obj = $youTubeService->videos->insert("status,snippet", $video,
-                                         array("data"=>file_get_contents( $uploadDirectory ), 
-                                        "mimeType" => "video/mp4"));
-    } catch(Google_ServiceException $e) {
-        print "Caught Google service Exception ".$e->getCode(). " message is ".$e->getMessage(). " <br>";
-        print "Stack trace is ".$e->getTraceAsString();
-    }
-} else {
-  $authUrl = $client->createAuthUrl();
-  print "<a href='$authUrl'>Connect Me!</a>";
-}
+//     try {
+//         $obj = $youTubeService->videos->insert("status,snippet", $video,
+//                                          array("data"=>file_get_contents( $uploadDirectory ), 
+//                                         "mimeType" => "video/mp4"));
+//     } catch(Google_ServiceException $e) {
+//         print "Caught Google service Exception ".$e->getCode(). " message is ".$e->getMessage(). " <br>";
+//         print "Stack trace is ".$e->getTraceAsString();
+//     }
+// } else {
+//   $authUrl = $client->createAuthUrl();
+//   print "<a href='$authUrl'>Connect Me!</a>";
+// }
